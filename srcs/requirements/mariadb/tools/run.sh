d3c98dab2a8d4eb9fae2122734cb244eb7134f45
ilahyani <lahyani799@gmail.com>
2022-12-30 15:47:46 +0100

    run.sh update

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-# export WORDPRESS_DB_HOST=db
-# export MYSQL_ROOT_PASSWORD=kamiodl7lib
-# export DATABASE=wordpress
-# export USER=wordpress
-# export USER_PASSWD=wordpress
-# export ADMIN=ilahyani
-# export ADMIN_PASSWD=admin123
+export WORDPRESS_DB_HOST=db
+export MYSQL_ROOT_PASSWORD=kamiodl7lib
+export DATABASE=wordpress
+export USER=wordpress
+export USER_PASSWD=wordpress
+export MYSQL_ALLOW_EMPTY_PASSWORD=yes
 
 service mysql start
 
@@ -16,8 +15,4 @@ echo "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${USER_PASSWD}';" | mysql
 
 echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER}'@'localhost' IDENTIFIED BY '${MYSQL_LOCAL_PASSWORD}';" | mysql -u root
 
-echo "CREATE USER '${ADMIN}'@'%' IDENTIFIED BY '${ADMIN_PASSWD}';" | mysql -u root
-
-echo "GRANT ALL PRIVILEGES ON *.* TO '${ADMIN}'@'%' IDENTIFIED BY '${ADMIN_PASSWD}';" | mysql -u root
-
-echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mysql -u root
