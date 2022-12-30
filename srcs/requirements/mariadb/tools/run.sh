e363ae81abc2a03ea76d0a00abcdc5e4613d4f37
ilahyani <lahyani799@gmail.com>
2022-12-30 10:41:03 +0100

    env update

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-export DATABASE=wpdatabase
-export LOCAL=local
-export LOCAL_PASSWD=local
-export ADMIN=ilahyani
-export ADMIN_PASSWD=admin
-export MYSQL_ALLOW_EMPTY_PASSWORD=yes
+# export WORDPRESS_DB_HOST=db
+# export MYSQL_ROOT_PASSWORD=kamiodl7lib
+# export DATABASE=wordpress
+# export USER=wordpress
+# export USER_PASSWD=wordpress
+# export ADMIN=ilahyani
+# export ADMIN_PASSWD=admin123
 
 service mysql start
 
 echo "CREATE DATABASE ${DATABASE};" | mysql -u root
 
-echo "CREATE USER '${LOCAL}'@'localhost' IDENTIFIED BY '${LOCAL_PASSWD}';" | mysql -u root
+echo "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${USER_PASSWD}';" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${LOCAL}'@'localhost' IDENTIFIED BY '${MYSQL_LOCAL_PASSWORD}';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER}'@'localhost' IDENTIFIED BY '${MYSQL_LOCAL_PASSWORD}';" | mysql -u root
 
 echo "CREATE USER '${ADMIN}'@'%' IDENTIFIED BY '${ADMIN_PASSWD}';" | mysql -u root
 
