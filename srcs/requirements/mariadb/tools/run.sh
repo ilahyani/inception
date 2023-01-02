2754b6184549915fec7325be4b110c23262891e1
ilahyani <lahyani799@gmail.com>
2023-01-02 15:37:02 +0100

    wordpress setup

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,18 +1,29 @@
 #! /bin/bash
 
-export WORDPRESS_DB_HOST=db
-export MYSQL_ROOT_PASSWORD=kamiodl7lib
-export DATABASE=wordpress
-export USER=wordpress
-export USER_PASSWD=wordpress
-export MYSQL_ALLOW_EMPTY_PASSWORD=yes
+# export WORDPRESS_DB_HOST=172.17.0.2:3306
+# export MYSQL_ROOT_PASSWORD=rootpasswd
+# export DATABASE=wordpress
+# export USER=wordpress
+# export USER_PASSWD=wordpress
+# export DOMAIN_NAME=ilahyani.42.fr
+# export WP_TITLE=website
+# export WP_ADMIN=ilahyani
+# export WP_ADMIN_PWD=adminpasswd
+# export WP_ADMIN_EMAIL=lahyani799@gmail.com
+# export WP_USER=smail
+# export WP_EMAIL=ilahyani@student.1337.ma 
+# export WP_USER_PWD=smailpasswd
+
+sed -i 's/127.0.0.1/0.0.0.0/g' etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
 echo "CREATE DATABASE ${DATABASE};" | mysql -u root
 
-echo "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${USER_PASSWD}';" | mysql -u root
+echo "CREATE USER '${USER}'@'%' IDENTIFIED BY '${USER_PASSWD}';" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER}'@'localhost' IDENTIFIED BY '${MYSQL_LOCAL_PASSWORD}';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER}'@'%' IDENTIFIED BY '${USER_PASSWD}';" | mysql -u root
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" | mysql -u root
+
+mysqld_safe
---
+++ srcs/requirements/wordpress/tools/run.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+chmod +x /usr/local/bin/wp
+
+mkdir -p var/www/html
+
+cd /var/www/html/
+
+wp core download --allow-root
+
+wp core config --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWD} --dbhost=${WORDPRESS_DB_HOST} --allow-root --skip-check
+
+wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} \
+    --skip-email --allow-root --path=var/www/html --allow-root
+
+wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_USER_PWD} --allow-root
+
+mkdir /run/php
+
+/usr/sbin/php-fpm7.3 -F
