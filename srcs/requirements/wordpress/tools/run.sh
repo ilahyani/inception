840506a516c6633108a358df1a87ba41764aff3b
ilahyani <ilahyani@student.1337.ma>
2023-01-17 13:56:02 +0100

    remove allow-root flag

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -14,24 +14,24 @@ chmod +x /usr/local/bin/wp
 
 mkdir -p var/www/html
 
-runuser -u www-data -- wp core download --allow-root --path=/var/www/html
+runuser -u www-data -- wp core download --path=/var/www/html
 
-runuser -u www-data -- wp core config --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWD} --dbhost=${WORDPRESS_DB_HOST} --allow-root --path=/var/www/html --skip-check
+runuser -u www-data -- wp core config --dbname=${DATABASE} --dbuser=${USER} --dbpass=${USER_PASSWD} --dbhost=${WORDPRESS_DB_HOST} --path=/var/www/html --skip-check
 
 runuser -u www-data -- wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} \
-    --skip-email --allow-root --path=/var/www/html
+    --skip-email --path=/var/www/html
 
-runuser -u www-data -- wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_USER_PWD} --allow-root --path=/var/www/html
+runuser -u www-data -- wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_USER_PWD} --path=/var/www/html
 
-runuser -u www-data -- wp plugin update --all --allow-root --path=/var/www/html
+runuser -u www-data -- wp plugin update --all --path=/var/www/html
 
-runuser -u www-data -- wp plugin install redis-cache --activate --allow-root --path=/var/www/html
+runuser -u www-data -- wp plugin install redis-cache --activate --path=/var/www/html
 
 runuser -u www-data -- wp config set WP_REDIS_HOST redis --path=/var/www/html
 
 runuser -u www-data -- wp config set WP_REDIS_PORT 6379 --path=/var/www/html
 
-runuser -u www-data -- wp redis enable --allow-root --path=/var/www/html
+runuser -u www-data -- wp redis enable --path=/var/www/html
 
 chown -R www-data:www-pub /var/www
 
